5c48ba4a1982792dd74fb4c139f7fe412c4e8974
Reed <reedjmerrill@gmail.com>
2025-02-15 20:55:52 -0700

    constant, operator, and vcs

--- lua/ayu/colors.lua
+++ lua/ayu/colors.lua
@@ -7,7 +7,6 @@ local colors = {
 }
 
 --- Generate colors based `vim.o.background` and a variant.
----@param mirage boolean: Whether to use `mirage` variant when `vim.o.background` is dark.
 function colors.generate()
   colors.accent = '#E6B450'
   colors.bg = '#0B0E14'
@@ -23,8 +22,8 @@ function colors.generate()
   colors.keyword = '#FF8F40'
   colors.special = '#E6B673'
   colors.comment = '#636A72'
-  colors.constant = '#D2A6FF'
-  colors.operator = '#F29668'
+  colors.constant = '#c5ffa1'
+  colors.operator = '#fdffa1'
   colors.error = '#D95757'
   colors.lsp_parameter = '#CB9FF8'
 
@@ -40,8 +39,8 @@ function colors.generate()
   colors.guide_active = '#3C414A'
   colors.guide_normal = '#BFBDB6'
 
-  colors.vcs_added = '#7FD962'
-  colors.vcs_modified = '#73B8FF'
+  colors.vcs_added = '#AAD94C'
+  colors.vcs_modified = '#E6B450'
   colors.vcs_removed = '#F26D78'
 
   colors.vcs_added_bg = '#1D2214'
