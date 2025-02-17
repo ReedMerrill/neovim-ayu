164c7f6f2655f0cdd3e9f130c38411eab3c6e9a7
Reed <reedjmerrill@gmail.com>
2025-02-17 15:56:33 -0700

    full colors overwrite

--- lua/ayu/colors.lua
+++ lua/ayu/colors.lua
@@ -1,3 +1,16 @@
+local peach = '#ff9980'
+local red = '#F07178'
+local orange = '#FF8F40'
+local yellow = '#FFB454'
+local light_yellow = '#ffea80'
+local green = '#AAD94C'
+local blue_green = '#95E6CB'
+local blue = '#59C2FF'
+local dark_blue = '#528afa'
+local gray = '#636A72'
+local white = '#FFFFFF'
+local black = '#000000'
+
 local colors = {
   white = '#FFFFFF',
   black = '#000000',
@@ -8,24 +21,24 @@ local colors = {
 
 --- Generate colors based `vim.o.background` and a variant.
 function colors.generate()
-  colors.accent = '#E6B450'
-  colors.bg = '#0B0E14'
+  colors.accent = light_yellow
+  colors.bg = black
   colors.fg = '#BFBDB6'
   colors.ui = '#565B66'
 
-  colors.tag = '#39BAE6'
-  colors.func = '#FFB454'
-  colors.entity = '#59C2FF'
-  colors.string = '#AAD94C'
-  colors.regexp = '#95E6CB'
-  colors.markup = '#F07178'
-  colors.keyword = '#FF8F40'
-  colors.special = '#ffbb8a'
-  colors.comment = '#636A72'
-  colors.constant = '#E6B450'
-  colors.operator = '#F07178'
+  colors.tag = blue
+  colors.func = green
+  colors.entity = blue_green
+  colors.string = yellow
+  colors.regexp = yellow
+  colors.markup = red
+  colors.keyword = peach
+  colors.special = peach
+  colors.comment = gray
+  colors.constant = light_yellow
+  colors.operator = light_yellow
   colors.error = '#D95757'
-  colors.lsp_parameter = '#9d3bff'
+  colors.lsp_parameter = peach
 
   colors.line = '#11151C'
   colors.panel_bg = '#0F131A'
@@ -39,15 +52,15 @@ function colors.generate()
   colors.guide_active = '#3C414A'
   colors.guide_normal = '#BFBDB6'
 
-  colors.vcs_added = '#AAD94C'
-  colors.vcs_modified = '#E6B450'
-  colors.vcs_removed = '#F26D78'
+  colors.vcs_added = green
+  colors.vcs_modified = yellow
+  colors.vcs_removed = red
 
   colors.vcs_added_bg = '#1D2214'
   colors.vcs_removed_bg = '#2D2220'
 
   colors.fg_idle = '#565B66'
-  colors.warning = '#FF8F40'
+  colors.warning = orange
 end
 
 return colors
