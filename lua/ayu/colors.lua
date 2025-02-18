5202ce628d4bd172929fba68bdee66e4e044007d
Reed <reedjmerrill@gmail.com>
2025-02-17 19:23:36 -0700

    cleanup

--- lua/ayu/colors.lua
+++ lua/ayu/colors.lua
@@ -1,67 +1,67 @@
-local peach = '#ff9980'
-local red = '#F07178'
-local orange = '#FF8F40'
-local yellow = '#ffdd00'
-local pastel_green = '#e8fca4'
-local green = '#AAD94C'
-local blue_green = '#95E6CB'
-local blue = '#59C2FF'
-local deep_blue = '#528afa'
-local dark_blue = '#000dff'
-local gray = '#636A72'
-local white = '#FFFFFF'
-local black = '#000000'
+local peach = "#ff9980"
+local red = "#F07178"
+local orange = "#FF8F40"
+local yellow = "#ffdd00"
+local pastel_green = "#e8fca4"
+local green = "#AAD94C"
+local blue_green = "#95E6CB"
+local blue = "#59C2FF"
+local deep_blue = "#528afa"
+local dark_blue = "#000dff"
+local gray = "#636A72"
+local white = "#FFFFFF"
+local black = "#000000"
 
 local colors = {
-  white = '#FFFFFF',
-  black = '#000000',
+	white = "#FFFFFF",
+	black = "#000000",
 
-  -- This color is not part of `ayu` but matches the default style applied in VSCode.
-  lsp_inlay_hint = '#969696',
+	-- This color is not part of `ayu` but matches the default style applied in VSCode.
+	lsp_inlay_hint = "#969696",
 }
 
 --- Generate colors based `vim.o.background` and a variant.
 function colors.generate()
-  colors.accent = pastel_green
-  colors.bg = black
-  colors.fg = white
-  colors.ui = '#565B66'
+	colors.accent = pastel_green
+	colors.bg = black
+	colors.fg = white
+	colors.ui = "#565B66"
 
-  colors.tag = blue
-  colors.func = deep_blue
-  colors.entity = pastel_green
-  colors.string = green
-  colors.regexp = green
-  colors.markup = red
-  colors.keyword = blue_green
-  colors.special = blue_green
-  colors.comment = gray
-  colors.constant = pastel_green
-  colors.operator = pastel_green
-  colors.error = red
-  colors.lsp_parameter = peach
+	colors.tag = blue
+	colors.func = deep_blue
+	colors.entity = pastel_green
+	colors.string = green
+	colors.regexp = green
+	colors.markup = red
+	colors.keyword = blue_green
+	colors.special = blue_green
+	colors.comment = gray
+	colors.constant = pastel_green
+	colors.operator = pastel_green
+	colors.error = red
+	colors.lsp_parameter = peach
 
-  colors.line = '#11151C'
-  colors.panel_bg = '#0F131A'
-  colors.panel_shadow = '#05070A'
-  colors.panel_border = '#000000'
-  colors.gutter_normal = '#454B55'
-  colors.gutter_active = '#626975'
-  colors.selection_bg = '#1B3A5B'
-  colors.selection_inactive = '#122132'
-  colors.selection_border = dark_blue
-  colors.guide_active = '#3C414A'
-  colors.guide_normal = '#BFBDB6'
+	colors.line = "#11151C"
+	colors.panel_bg = "#0F131A"
+	colors.panel_shadow = "#05070A"
+	colors.panel_border = "#000000"
+	colors.gutter_normal = "#454B55"
+	colors.gutter_active = "#626975"
+	colors.selection_bg = "#1B3A5B"
+	colors.selection_inactive = "#122132"
+	colors.selection_border = dark_blue
+	colors.guide_active = "#3C414A"
+	colors.guide_normal = "#BFBDB6"
 
-  colors.vcs_added = green
-  colors.vcs_modified = yellow
-  colors.vcs_removed = red
+	colors.vcs_added = green
+	colors.vcs_modified = yellow
+	colors.vcs_removed = red
 
-  colors.vcs_added_bg = '#1D2214'
-  colors.vcs_removed_bg = '#2D2220'
+	colors.vcs_added_bg = "#1D2214"
+	colors.vcs_removed_bg = "#2D2220"
 
-  colors.fg_idle = '#565B66'
-  colors.warning = orange
+	colors.fg_idle = "#565B66"
+	colors.warning = orange
 end
 
 return colors
